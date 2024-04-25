5a0b50ee203c7d555955aaedb066666756a43e4d
Aaron Burciaga <146780951+BurciagaAA128@users.noreply.github.com>
2024-04-25 11:50:38 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,104 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const Myapp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "Flutter Pockets"),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
   final String title;
-  const MyHomePage({super.key, required this.title});  
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> imgList = [
+    "https://images.unsplash.com/photo-1535910088135-abe86476a866?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1544281153-6603be88b354?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1561079028-9f0935ab1edc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1556340006-ffa792cc226a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1547216156-eeacd848d141?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1545062080-a71640ea75a1?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  ];
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: imgList
+                  .map((e) => Center(
+                        child: Center(
+                          child: Image.network(e),
+                        ),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                  initialPage: 0,
+                  height: 200,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 2),
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.3,
+                  onPageChanged: (value, _) {
+                    setState(() {
+                      _currentPage = value;
+                    });
+                  }),
+            ),
+            buildCarouselIndicator()
+          ],
         ),
       ),
+    );
+  }
+
+  buildCarouselIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Row(
+            children: [
+              for (int i = 0; i < imgList.length; i++)
+                Container(
+                  margin: EdgeInsets.all(5),
+                  height: i == _currentPage ? 7 : 5,
+                  width: i == _currentPage ? 7 : 5,
+                  decoration: BoxDecoration(
+                      color: i == _currentPage ? Colors.black : Colors.grey,
+                      shape: BoxShape.circle),
+                )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
